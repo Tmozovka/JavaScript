35c9ea77eb3556e0d4e7b465ddb4d38036783ee8
Mazouka <d0293581@kampi00000305.dm.int>
2019-07-12 10:51:43 +0200

    add clock

--- clock/script.js
+++ clock/script.js
@@ -5,14 +5,13 @@ function setDate(){
     const sec = now.getSeconds();
     const min = now.getMinutes();
     const hour = now.getHours();
-    
+
     const sec_hand = document.querySelector(".sec-hand");
     const min_hand = document.querySelector(".min-hand");
     const hour_hand = document.querySelector(".hour-hand");
 
     const sec_degree = (sec / 60) * 360 + 90;
     sec_hand.style.transform = `rotate(${sec_degree}deg)`;
-
 
     const min_degree = (min / 60) * 360 + 90;
     min_hand.style.transform = `rotate(${min_degree}deg)`;
